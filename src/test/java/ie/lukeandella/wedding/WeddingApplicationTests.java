4d426637eff60cd6132f09407bd86e0e34ef804c
lukemsmyth <80278539+lukemsmyth@users.noreply.github.com>
2021-06-28 19:00:18 +0100

    First commit

---
+++ src/test/java/ie/lukeandella/wedding/WeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.lukeandella.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
