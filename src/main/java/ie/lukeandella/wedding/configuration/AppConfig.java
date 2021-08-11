cc002f8a97e8a143f8d084fd1719a14e8879b2e5
lukemsmyth <80278539+lukemsmyth@users.noreply.github.com>
2021-08-11 17:01:57 +0100

    More small fixes.

--- src/main/java/ie/lukeandella/wedding/configuration/AppConfig.java
+++ src/main/java/ie/lukeandella/wedding/configuration/AppConfig.java
@@ -34,69 +34,69 @@ public class AppConfig {
     {
         return args -> {
 
-            /*
-             * Initialise the application with 3 users.
-             */
-
-            //Make a password
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            String password = encoder.encode("0000");
-            //Make admin
-            Role adminRole = new Role("ADMIN");
-            User adminUser = new User("admin@x.com", password, true);
-            adminUser.setEmail("admin@x.com");
-            adminUser.addRole(adminRole);
-            userRepository.save(adminUser);
-            roleRepository.save(adminRole);
-            //Make member
-            Role memberRole = new Role("MEMBER");
-            User memberUser = new User("member@x.com", password, true);
-            memberUser.setEmail("member@x.com");
-            memberUser.addRole(memberRole);
-            userRepository.save(memberUser);
-            roleRepository.save(memberRole);
-            //Make visitor
-            Role visitorRole = new Role("VISITOR");
-            User visitorUser = new User("visitor@x.com", password, true);
-            visitorUser.setEmail("visitor@x.com");
-            visitorUser.addRole(visitorRole);
-            userRepository.save(visitorUser);
-            roleRepository.save(visitorRole);
-
-            /*
-             * Initialise the application with some throwaway data.
-             */
-            Itinerary it1 = new Itinerary(LocalDateTime.of(2020, 6, 15, 12, 30),"Sandycove", "Swim at the Forty Foot", "Going for a dip at the Forty Foot.", LocalDateTime.now());
-            itineraryRepository.save(it1);
-            Itinerary it2 = new Itinerary(LocalDateTime.of(2020, 6, 16, 13, 30),"Clontarf", "Walk in the park", "Going for a walk in St. Anne's Park.", LocalDateTime.now());
-            itineraryRepository.save(it2);
-            Itinerary it3 = new Itinerary(LocalDateTime.of(2020, 6, 17, 18, 30),"Dublin", "Night on the beer", "Going for a night on the beer.", LocalDateTime.now());
-            itineraryRepository.save(it3);
-            Itinerary it4 = new Itinerary(LocalDateTime.of(2020, 6, 18, 18, 30),"Smock Alley Theatre", "The Big Day", "Wedding ceremony.", LocalDateTime.now());
-            itineraryRepository.save(it4);
-
-            String smokedSalmon = "Smoked salmon is a preparation of salmon, typically a fillet that has been cured and hot or cold smoked. Due to its moderately high price, smoked salmon is considered a delicacy.";
-            Faq faq1 = new Faq("Are children invited?", smokedSalmon, LocalDateTime.now());
-            faqRepository.save(faq1);
-            Faq faq2 = new Faq("Are dogs invited?", smokedSalmon, LocalDateTime.now());
-            faqRepository.save(faq2);
-            Faq faq3 = new Faq("Are cats invited?", smokedSalmon, LocalDateTime.now());
-            faqRepository.save(faq3);
-            Faq faq4 = new Faq("Are goats invited?", smokedSalmon, LocalDateTime.now());
-            faqRepository.save(faq4);
-            Faq faq5 = new Faq("Am I invited?", smokedSalmon, LocalDateTime.now());
-            faqRepository.save(faq5);
-
-            Gift tomato = new Gift("Tomato", "One ripe tomoato.", 1000.00, "null", "http://example.com/", false, 0, true);
-            giftRepository.save(tomato);
-            Gift potatoes = new Gift("Potatoes", "A sack of spuds", 99.99, "N/A", "http://example.com/", false, 0, true);
-            giftRepository.save(potatoes);
-            Gift oranges = new Gift("Oranges", "A nice bag of oranges.", 100.00, "null", "http://example.com/", true, 0, true);
-            giftRepository.save(oranges);
-            Gift apples = new Gift("Apples", "A nice bag of apples.", 100.00, "null", "http://example.com/", false, 0, true);
-            giftRepository.save(apples);
-            Gift tofu = new Gift("Tofu", "A packet of extra-firm tofu.", 100.00, "null", "http://example.com/", false, 0, true);
-            giftRepository.save(tofu);
+//            /*
+//             * Initialise the application with 3 users.
+//             */
+//
+//            //Make a password
+//            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//            String password = encoder.encode("0000");
+//            //Make admin
+//            Role adminRole = new Role("ADMIN");
+//            User adminUser = new User("admin@x.com", password, true);
+//            adminUser.setEmail("admin@x.com");
+//            adminUser.addRole(adminRole);
+//            userRepository.save(adminUser);
+//            roleRepository.save(adminRole);
+//            //Make member
+//            Role memberRole = new Role("MEMBER");
+//            User memberUser = new User("member@x.com", password, true);
+//            memberUser.setEmail("member@x.com");
+//            memberUser.addRole(memberRole);
+//            userRepository.save(memberUser);
+//            roleRepository.save(memberRole);
+//            //Make visitor
+//            Role visitorRole = new Role("VISITOR");
+//            User visitorUser = new User("visitor@x.com", password, true);
+//            visitorUser.setEmail("visitor@x.com");
+//            visitorUser.addRole(visitorRole);
+//            userRepository.save(visitorUser);
+//            roleRepository.save(visitorRole);
+//
+//            /*
+//             * Initialise the application with some throwaway data.
+//             */
+//            Itinerary it1 = new Itinerary(LocalDateTime.of(2020, 6, 15, 12, 30),"Sandycove", "Swim at the Forty Foot", "Going for a dip at the Forty Foot.", LocalDateTime.now());
+//            itineraryRepository.save(it1);
+//            Itinerary it2 = new Itinerary(LocalDateTime.of(2020, 6, 16, 13, 30),"Clontarf", "Walk in the park", "Going for a walk in St. Anne's Park.", LocalDateTime.now());
+//            itineraryRepository.save(it2);
+//            Itinerary it3 = new Itinerary(LocalDateTime.of(2020, 6, 17, 18, 30),"Dublin", "Night on the beer", "Going for a night on the beer.", LocalDateTime.now());
+//            itineraryRepository.save(it3);
+//            Itinerary it4 = new Itinerary(LocalDateTime.of(2020, 6, 18, 18, 30),"Smock Alley Theatre", "The Big Day", "Wedding ceremony.", LocalDateTime.now());
+//            itineraryRepository.save(it4);
+//
+//            String smokedSalmon = "Smoked salmon is a preparation of salmon, typically a fillet that has been cured and hot or cold smoked. Due to its moderately high price, smoked salmon is considered a delicacy.";
+//            Faq faq1 = new Faq("Are children invited?", smokedSalmon, LocalDateTime.now());
+//            faqRepository.save(faq1);
+//            Faq faq2 = new Faq("Are dogs invited?", smokedSalmon, LocalDateTime.now());
+//            faqRepository.save(faq2);
+//            Faq faq3 = new Faq("Are cats invited?", smokedSalmon, LocalDateTime.now());
+//            faqRepository.save(faq3);
+//            Faq faq4 = new Faq("Are goats invited?", smokedSalmon, LocalDateTime.now());
+//            faqRepository.save(faq4);
+//            Faq faq5 = new Faq("Am I invited?", smokedSalmon, LocalDateTime.now());
+//            faqRepository.save(faq5);
+//
+//            Gift tomato = new Gift("Tomato", "One ripe tomoato.", 1000.00, "null", "http://example.com/", false, 0, true);
+//            giftRepository.save(tomato);
+//            Gift potatoes = new Gift("Potatoes", "A sack of spuds", 99.99, "N/A", "http://example.com/", false, 0, true);
+//            giftRepository.save(potatoes);
+//            Gift oranges = new Gift("Oranges", "A nice bag of oranges.", 100.00, "null", "http://example.com/", true, 0, true);
+//            giftRepository.save(oranges);
+//            Gift apples = new Gift("Apples", "A nice bag of apples.", 100.00, "null", "http://example.com/", false, 0, true);
+//            giftRepository.save(apples);
+//            Gift tofu = new Gift("Tofu", "A packet of extra-firm tofu.", 100.00, "null", "http://example.com/", false, 0, true);
+//            giftRepository.save(tofu);
 
         };
     }
